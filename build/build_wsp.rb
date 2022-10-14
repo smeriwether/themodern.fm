a1c6438a360199643ff0dd16421643a0c86d0d60
Stephen Meriwether <stephen.meriwether@icloud.com>
2022-10-14 18:38:42 -0400

    Add eliminator picks to wsp picks page

--- build/build_wsp.rb
+++ build/build_wsp.rb
@@ -3,27 +3,32 @@ def jd_picks_2022
     [
       { pick: "Vikings -7.5 vs. Bears", winner: false },
       { pick: "Titans -1.5 at Commanders", winner: true },
-      { pick: "49ers -6.5 at Panthers", winner: true }
+      { pick: "49ers -6.5 at Panthers", winner: true },
+      { eliminator_pick: 'Packers', winner: false }
     ],
     [
       { pick: "Vikings -3 at Saints", winner: false },
       { pick: "Texans +5 vs. Chargers", winner: false },
-      { pick: "Jags +6.5 at Eagles", winner: false }
+      { pick: "Jags +6.5 at Eagles", winner: false },
+      { eliminator_pick: 'Giants', winner: true }
     ],
     [
       { pick: "Commanders -6.5 vs Eagles", winner: false },
       { pick: "Vikings -6 vs Lions", winner: false },
-      { pick: "Texans +3 at Bears", "push": true }
+      { pick: "Texans +3 at Bears", "push": true },
+      { eliminator_pick: 'Saints', winner: false }
     ],
     [
       { pick: "Ravens -3.5 vs Dolphins", winner: false },
       { pick: "Steelers +2 vs Patriots", winner: false },
-      { pick: "Packers -9.5 vs Bears", winner: true }
+      { pick: "Packers -9.5 vs Bears", winner: true },
+      { eliminator_pick: 'Lions', winner: true }
     ],
     [
       { pick: "Jags +3 at Commanders", winner: false },
       { pick: "Chiefs -6.5 at Cardinals", winner: true },
-      { pick: "Panthers -1 vs Browns", winner: false }
+      { pick: "Panthers -1 vs Browns", winner: false },
+      { eliminator_pick: 'Dolphins', winner: true }
     ],
   ]
 end
@@ -33,27 +38,32 @@ def stephen_picks_2022
     [
       { pick: "Cowboys +5.5 at Rams", winner: true },
       { pick: "Falcons +10 at Bucs", winner: true },
-      { pick: "Seahawks +5.5 at Saints", winner: false }
+      { pick: "Seahawks +5.5 at Saints", winner: false },
+      { eliminator_pick: 'Jags', winner: false }
     ],
     [
       { pick: "Vikings -3.5 at Saints", winner: false },
       { pick: "Chiefs +1 at Bucs", winner: true },
-      { pick: "Bears +3 at Giants", winner: false }
+      { pick: "Bears +3 at Giants", winner: false },
+      { eliminator_pick: 'Packers', winner: true }
     ],
     [
       { pick: "Ravens -3 at Patriots", winner: true },
       { pick: "Chiefs -5.5 at Colts", winner: false },
-      { pick: "Bills -5.5 at Dolphins", winner: false }
+      { pick: "Bills -5.5 at Dolphins", winner: false },
+      { eliminator_pick: 'Raiders', winner: false }
     ],
     [
       { pick: "Ravens -3.5 vs Dolphins", winner: false },
       { pick: "Bengals -7 at Cowboys", winner: false },
-      { pick: "Vikings +2.5 at Eagles", winner: false }
+      { pick: "Vikings +2.5 at Eagles", winner: false },
+      { eliminator_pick: 'Bengals', winner: false }
     ],
     [
       { pick: "Jags +3 at Commanders", winner: false },
       { pick: "Ravens -6.5 at Jets", winner: true },
-      { pick: "Panthers -1 vs Browns", winner: false }
+      { pick: "Panthers -1 vs Browns", winner: false },
+      { eliminator_pick: 'Panthers', winner: false }
     ],
   ]
 end
@@ -67,6 +77,7 @@ def wsp_picks_data
           name: "Week #{jd_picks_2022.length - idx}",
           jd: week.map do |pick|
             {
+              eliminator_pick: pick[:eliminator_pick],
               pick: pick[:pick],
               winner: pick[:winner],
               push: pick[:push]
@@ -74,6 +85,7 @@ def wsp_picks_data
           end,
           stephen: stephen_picks_2022[idx].map do |pick|
             {
+              eliminator_pick: pick[:eliminator_pick],
               pick: pick[:pick],
               winner: pick[:winner],
               push: pick[:push]
